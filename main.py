2f755117a3e885f974fe74009d90049e40ebd954
IrinaKonopl <irina.konolyanova@gmail.com>
2024-12-04 11:06:59 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+# 1st program
+print(5*9**0.5)
+
+# 2nd program
+print(99.9>99.8 and 1000!=1000.1)
+
+# 3d program
+print(2*2+2)
+print(2*(2+2))
+print(2*2+2==2*(2+2))
+
+#4th program
+s='123.456'
+print(int(float(s)*10)%10)
